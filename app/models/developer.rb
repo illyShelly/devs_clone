a49a83f2919e1258abd312c89271d5e384b8ee96
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-03-23 22:38:26 +0100

    Dev model - methods for view -> pluck, titleize, proc &:

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -5,7 +5,31 @@ class Developer < ApplicationRecord
   has_many :skills, through: :developer_skills
   has_many :projects
   has_many :bookings
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def skills_to_string
+    # skills.pluck(:name).map(&:titleize).join(" | ")
+    skills.pluck(:name).map { |skill| skill.titleize }.join(" | ")
+  end
+
 end
 
 # 1. relationships
 # 2. validation
+
+# pluck(*column_names) public
+# Use pluck as a shortcut to SELECT ONE OR MORE ATTRIBUTES without loading a bunch of records just to grab the attributes you want.
+# Person.pluck(:name) vs. Person.all.map(&:name)
+
+# titleize(word) public
+# Capitalizes all the words and REPLACES SOME CHARACTERS in the string to create a nicer looking title - as titlecase.
+# 'man from the boondocks'.titleize   # => "Man From The Boondocks"
+
+# Turns the symbol into a simple proc &:
+# The same as people.collect { |p| p.name } -> people.collect(&:name)
+
+# The same as people.select { |p| p.manager? }.collect { |p| p.salary }
+# people.select(&:manager?).collect(&:salary)
