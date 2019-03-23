ebe66d1e761f203652c69169b1013bef6775b3a1
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-03-23 21:11:46 +0100

    Seed - update for user authorization vs. origin file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,12 @@
 # require 'securerandom'
 
 puts "Cleaning up DB..."
+
 Project.destroy_all
 Skill.destroy_all
 Developer.destroy_all
+User.destroy_all
+Booking.destroy_all
 
 puts "Creating skills..."
 
@@ -24,8 +27,15 @@ skills = [ "ruby", "ruby on rails", "javascript", "react",
   "django", "haskell", "scala", "swift", "crystal", "drupal", "symfony", "sinatra" ]
 
 skills.each { |skill| Skill.create!(name: skill) }
-puts ''
+puts ""
 puts "Created #{Skill.count} skills!"
+puts ""
+
+puts "Creating users ..."
+puts ""
+
+user1 = User.create!(email: "dev@dev.dev", password: "password")
+user2 = User.create!(email: "de@de.de", password: "password")
 
 puts "Creating devs..."
 
@@ -35,6 +45,7 @@ torvalds = Developer.new(
   github_username: "torvalds",
   avatar_url: "https://avatars0.githubusercontent.com/u/1024025?v=4",
   hourly_rate: 450,
+  user_id: User.all.sample.id,
   bio: "Linus Benedict Torvalds is a Finnish–American software engineer who is the creator, and historically, the principal developer of the Linux kernel, which became the kernel for many Linux distributions and operating systems such as Android and Chrome OS. He also created the distributed version control system Git and the diving logging and planning software Subsurface. He was honoured, along with Shinya Yamanaka, with the 2012 Millennium Technology Prize by the Technology Academy Finland \"in recognition of his creation of a new open source operating system for computers leading to the widely used Linux kernel\". He is also the recipient of the 2014 IEEE Computer Society Computer Pioneer Award and the 2018 IEEE Masaru Ibuka Consumer Electronics Award."
   )
 %w(c c++).each do |s|
@@ -48,6 +59,7 @@ wharton = Developer.new(
   github_username: "jakewharton",
   avatar_url: "https://avatars0.githubusercontent.com/u/66577?s=400&v=4",
   hourly_rate: 350,
+  user_id: User.all.sample.id,
   bio: "Programmer. Web developer. Open source practitioner and contributor. Android enthusiast."
 )
 %w(kotlin java html javascript css).each do |s|
@@ -61,6 +73,7 @@ evan = Developer.new(
   github_username: "yyx990803",
   avatar_url: "https://avatars3.githubusercontent.com/u/499550?s=400&v=4",
   hourly_rate: 420,
+  user_id: User.all.sample.id,
   bio: "Design. Hack. Imagine. Front-end engineering, UI/UX & creative coding."
 )
 %w(javascript vue html css php python).each do |s|
@@ -74,6 +87,7 @@ tj = Developer.new(
   github_username: "tj",
   avatar_url: "https://avatars2.githubusercontent.com/u/25254?s=400&v=4",
   hourly_rate: 390,
+  user_id: User.all.sample.id,
   bio: "Founder of Apex\r\nhttps://apex.sh, a non-startup.\r\nmedium.com/@tjholowaychuk • twitter.com/tjholowaychuk • tjholowaychuk.com"
 )
 %w(javascript go html).each do |s|
@@ -87,6 +101,7 @@ sindresorhus = Developer.new(
   github_username: "sindresorhus",
   avatar_url: "https://avatars3.githubusercontent.com/u/170270?s=400&v=4",
   hourly_rate: 380,
+  user_id: User.all.sample.id,
   bio: "Maker · Full-Time Open-Sourcerer · Into Swift and Node.js"
 )
 %w(javascript swift node.js typescript css html).each do |s|
@@ -100,6 +115,7 @@ defunkt = Developer.new(
   github_username: "defunkt",
   avatar_url: "https://avatars2.githubusercontent.com/u/2?s=400&v=4",
   hourly_rate: 320,
+  user_id: User.all.sample.id,
   bio: "Chris Wanstrath is an American technology entrepreneur. He is the co-founder and CEO of GitHub, a social network centred around software development, which he created with Tom Preston-Werner in 2008. Before starting GitHub, he worked with CNET on GameSpot. According to Forbes, his net worth is estimated to be $1.4 billion and he is listed in Forbes Magazine in America's richest entrepreneurs under 40, as well as Fortune Magazine's 40 under 40. He was one of the key speakers at NASA's open source summit. He was named in CNBC's Disruptor 50 list."
 )
 %w(ruby javascript coffeescript python c).each do |s|
@@ -113,6 +129,7 @@ getify = Developer.new(
   github_username: "getify",
   avatar_url: "https://avatars0.githubusercontent.com/u/150330?s=400&v=4",
   hourly_rate: 370,
+  user_id: User.all.sample.id,
   bio: "Kyle Simpson is an evangelist of the open web, passionate about all things JavaScript. He writes books, teaches JavaScript, speaks, and contributes to the world of OSS."
 )
 %w(javascript).each do |s|
@@ -126,6 +143,7 @@ jlord = Developer.new(
   github_username: "jlord",
   avatar_url: "https://avatars0.githubusercontent.com/u/1305617?s=400&v=4",
   hourly_rate: 260,
+  user_id: User.all.sample.id,
   bio: "Jessica is an urban designer turned open web developer—which isn't as odd as it sounds. She's a backend engineer at Splice. Before that she worked on a bunch of things at GitHub, last was starting the Electron team. The year that changed everything was being a Code for America fellow in 2012. She did the fellowship after working at the City of Boston's Urban Design Technology group and an architecture firm. Before that she was in college, Georgia Tech, graduating with an architecture degree."
 )
 %w(javascript css html node.js).each do |s|
@@ -139,6 +157,7 @@ grider = Developer.new(
   github_username: "stephengrider",
   avatar_url: "https://avatars3.githubusercontent.com/u/5003903?s=400&v=4",
   hourly_rate: 320,
+  user_id: User.all.sample.id,
   bio: "Stephen Grider has been building complex Javascript front ends for top corporations in the San Francisco Bay Area. With an innate ability to simplify complex topics, Stephen has been mentoring engineers beginning their careers in software development for years, and has now expanded that experience onto Udemy, authoring the highest rated React course. He teaches on Udemy to share the knowledge he has gained with other software engineers. Invest in yourself by learning from Stephen's published courses."
 )
 %w(javascript css html elixir react node.js vue).each do |s|
@@ -152,6 +171,7 @@ dhh = Developer.new(
   github_username: "dhh",
   avatar_url: "https://avatars3.githubusercontent.com/u/2741?s=400&v=4",
   hourly_rate: 520,
+  user_id: User.all.sample.id,
   bio: "David Heinemeier Hansson (born 15 October 1979; also known to the Ruby programming language and automobile racing communities as DHH) is a Danish programmer and the creator of the popular Ruby on Rails web development framework and the Instiki wiki. He is also a partner at the web-based software development firm Basecamp. Hansson co-wrote Agile Web Development with Rails with Dave Thomas in 2005 as part of The Facets of Ruby Series. He also co-wrote Getting Real, Rework, and Remote with Jason Fried."
 )
 %w(ruby ruby\ on\ rails javascript coffeescript).each do |s|
@@ -159,26 +179,13 @@ dhh = Developer.new(
 end
 dhh.save!
 
-
-rodloboz = Developer.new(
-  first_name: "Rui",
-  last_name: "Freitas",
-  github_username: "rodloboz",
-  avatar_url: "https://avatars3.githubusercontent.com/u/23458442?s=400&u=0a5cd87195bd5f117aceb8baecd075731190c112&v=4",
-  hourly_rate: 30,
-  bio: "Rui attended Le Wagon's coding bootcamp in Lisbon and has been teaching at Le Wagon ever since. He finds programming a blend between creativity and craftsmanship. As a programming language, Ruby is his first love, but he also enjoys working with JavaScript frameworks, such as Vuejs and React, and Elixir."
-)
-%w(ruby ruby\ on\ rails javascript react vue elixir phoenix crystal).each do |s|
-  rodloboz.skills << Skill.find_by_name(s)
-end
-rodloboz.save!
-
 askn = Developer.new(
   first_name: "Aşkın",
   last_name: "Gedik",
   github_username: "askn",
   avatar_url: "https://avatars2.githubusercontent.com/u/632820?s=400&v=4",
   hourly_rate: 210,
+  user_id: User.all.sample.id,
   bio: "Software Development Team Lead at @protelturkiye, #Rails and #CrystalLang developer."
 )
 %w(crystal ruby go ruby\ on\ rails).each do |s|
@@ -192,6 +199,7 @@ drujensen = Developer.new(
   github_username: "drujensen",
   avatar_url: "https://avatars3.githubusercontent.com/u/18718?s=400&v=4",
   hourly_rate: 190,
+  user_id: User.all.sample.id,
   bio: "20 years of experience in software development with expertise in implementing web-based applications and phone apps. Expert knowledge in many different platforms, languages, and frameworks. Extensive experience with the whole life-cycle of designing, implementing, deploying and maintaining a software application. Hands on technologist who thrives in environments focused on producing value to customers."
 )
 %w(ruby php crystal javascript).each do |s|
@@ -205,6 +213,7 @@ tenderlove = Developer.new(
   github_username: "tenderlove",
   avatar_url: "https://avatars2.githubusercontent.com/u/3124?s=400&v=4",
   hourly_rate: 330,
+  user_id: User.all.sample.id,
   bio: "His talent is inspiring people. He likes to do that by writing fun and interesting programs and sharing them with others for the express purposes of teaching. He is passionate about helping other people in my field, and he thinks the most effective way for me to impact the most developers is by working on Open Source software. He chooses to work on Ruby and Rails because improving Rails provides a large impact on the developer community. He believes he has the skills and experience to be one of the most effective framework and language developers."
 )
 %w(ruby ruby\ on\ rails c c++).each do |s|
@@ -218,6 +227,7 @@ eileencodes = Developer.new(
   github_username: "eileencodes",
   avatar_url: "https://avatars2.githubusercontent.com/u/1080678?s=400&v=4",
   hourly_rate: 390,
+  user_id: User.all.sample.id,
   bio: "Eileen M. Uchitelle Senior Systems Engineer at GitHub. Her focus is to ensure applications and code are secure and fast. She is an avid contributor to open source focusing most of her energy on improving the Ruby on Rails framework. She's is a member of the Rails Core team and the Rails Security team. Eileen is also a speaker and has presented at many conferences on performance, Active Record, and contributing to Rails."
 )
 %w(ruby ruby\ on\ rails c swift javascript go).each do |s|
@@ -225,25 +235,13 @@ eileencodes = Developer.new(
 end
 eileencodes.save!
 
-excid3 = Developer.new(
-  first_name: "Chris",
-  last_name: "Oliver",
-  github_username: "excid3",
-  avatar_url: "https://avatars1.githubusercontent.com/u/67093?s=400&v=4",
-  hourly_rate: 140,
-  bio: "Chris Oliver specializes in web development and user experience design with Ruby on Rails, Javascript, HTML5, and CSS3."
-)
-%w(ruby ruby\ on\ rails python javascript c++).each do |s|
-  excid3.skills << Skill.find_by_name(s)
-end
-excid3.save!
-
 seb = Developer.new(
   first_name: "Sébastien",
   last_name: "Saunier",
   github_username: "seb@saunier.me",
   avatar_url: "https://avatars0.githubusercontent.com/u/414418?s=400&v=4",
   hourly_rate: 170,
+  user_id: User.all.sample.id,
   bio: "Sébastien is a developer with 10+ years of experience with shipping technological products. He co-founded Le Wagon to bring technical skills to creative people teaching them to code and build their own product."
 )
 %w(ruby ruby\ on\ rails css html javascript react).each do |s|
@@ -257,6 +255,7 @@ boris = Developer.new(
   github_username: "papillard",
   avatar_url: "https://avatars0.githubusercontent.com/u/2471555?s=400&v=4",
   hourly_rate: 60,
+  user_id: User.all.sample.id,
   bio: "Passionate about education, Boris launched Le Wagon to bring technical skills to creative people and entrepreneurs. Le Wagon is now ranked #1 coding school in the world."
 )
 %w(html css).each do |s|
