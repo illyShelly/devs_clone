db98149aa4ba1e4f181628569d288a795980290e
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-03-23 23:33:36 +0100

    Added navbar scss if working just bootstrap, Users controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @developers = Developer.all.take(3)
+    @developers = Developer.all.take(6)
   end
 end
