b9ae2e9fb93a9ab663e0380153f49bc6613f4ff8
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-03-24 21:22:20 +0100

    flatpickr css, js, layout application

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,17 @@
 import "bootstrap";
+import './application.css';
+// import { toggleIcons } from '../components/bookmarking';
+import { toggleDateInputs } from '../components/booking';
+
+// const homePage = document.querySelector('.pages.home');
+// if (homePage) {
+//   toggleIcons();
+// }
+
+// const devIndexPage = document.querySelector('.developers.index');
+// if (devIndexPage) {
+//   toggleIcons();
+// }
+
+// flatpickr
+toggleDateInputs();
